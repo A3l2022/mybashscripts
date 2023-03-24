1bef0b82d95659c568e51442f2ecbec2e4ad4863
A3l2022 <pierrebilao328@gmail.com>
2023-03-24 19:38:40 -0400

    first script

---
+++ serverInv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This script helps get the cpu, ram, and disk capacity of a server.
+
+#Get cpu information:
+echo "CPU information"
+/proc/cpuinfo | grep "model name" |uniq
+echo ""
+
+#Get ram information:
+echo "RAM information"
+free -h | awk '/Mem:/ {print $2}'
+echo ""
+
+#Disk information:
+echo "Disk information"
+df -h | awk '$NF=="/"{printf "%d/%dGB (%s)\n", $3,$2,$5}'
+echo ""
